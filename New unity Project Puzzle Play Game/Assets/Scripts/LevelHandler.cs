40e19bcc08a60e11101c0d2f66278572432e46c7
Kurai-kun <1992kurai@gmail.com>
2017-01-28 19:45:47 +0000

    Build Final For Experimental Test ( Light GOne Not )
    Build Set To Web Browser Default

---
+++ New unity Project Puzzle Play Game/Assets/Scripts/LevelHandler.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+//Using XML 
+using System.Xml;
+//Using Lists
+using System.Collections.Generic;
+
+public class LevelHandler : MonoBehaviour {
+    XmlDocument levelDoc;
+    XmlNodeList levelist;
+    List<string> levelArray;
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+        levelArray = new List<string>();
+        levelDoc = new XmlDocument();
+
+        TextAsset xmlfile = Resources.Load("levels", typeof(TextAsset)) as TextAsset;
+        levelDoc.LoadXml(xmlfile.text);
+        levelist = levelDoc.GetElementsByTagName("level");
+        foreach (XmlNode leveldata in levelist)
+        {
+            XmlNodeList levelinfo = leveldata.ChildNodes;
+            foreach (XmlNode data in levelinfo)
+            {
+                if (data.Name == "setup")
+                {
+                    levelArray.Add(data.InnerText);
+                }
+            }
+
+        }
+
+        this.gameObject.GetComponent<Engine>().init(levelArray.Count);
+
+    }
+
+    public void loadLevel(int nr)
+    {
+
+        GameObject.Find("Canvas").GetComponent<Menu>().setLevelText(nr.ToString());
+
+        string[] levString = levelArray[nr - 1].Split(',');
+        foreach (string brick in levString)
+        {
+            GameObject.Find(brick).GetComponent<LightSwitch>().change();
+        }
+
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
+
---
+++ New unity Project Puzzle Play Game/Assets/Scripts/Menu.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+//Adding For Unity New UI Feature S
+using UnityEngine.UI;
+
+public class Menu : MonoBehaviour
+{
+    private int anim1 = Animator.StringToHash("mainState");
+    private int anim2 = Animator.StringToHash("hudState");
+
+    private Animator animator1;
+    private Animator animator2;
+
+    // Use this for initialization
+    void Start()
+    {
+
+        animator1 = this.transform.FindChild("Main").GetComponent<Animator>();
+        animator2 = this.transform.FindChild("Hud").GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void setLevelText(string name)
+    {
+        this.transform.FindChild("Hud").transform.FindChild("Panel").transform.FindChild("headLine").GetComponent<Text>().text = "LVL 00" + name;
+    }
+
+
+    public void animateMenu()
+    {
+        animator1.SetInteger(anim1, 1);
+        animator2.SetInteger(anim2, 1);
+    }
+
+
+}
